f9db0e64d168e439b39c0a1ec39ac7c4dfb0410c
Marco Ceccarelli <marco.ceccarelli.code@gmail.com>
2025-04-09 15:41:12 +0200

    organization

--- Wordle.py
+++ Wordle.py
@@ -1,37 +1,38 @@
+from kivy.config import Config
+Config.set('graphics', 'resizable', '0')
+Config.set('graphics', 'width', '340')
+Config.set('graphics', 'height', '405')
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+
 from kivy.app import App
-from kivy.uix.textinput import TextInput
-from kivy.uix.boxlayout import BoxLayout
-import random
+from kivy.uix.screenmanager import ScreenManager, FadeTransition
 
-class Wordle(App):
+from Dictionary import Dictionary
+from Screens import MainMenuScreen, GameScreen, VictoryScreen, DefeatScreen
 
-	def build(self):
-		layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+class WordleApp(App):
 
-		# Create two TextInput widgets
-		self.textinput1 = TextInput(hint_text="Type here (1)", multiline=False)
-		self.textinput2 = TextInput(hint_text="Type here (2)", multiline=False)
+	def build(self, dictionary_file = "test.txt"):
 
-		# Bind the on_text event to switch focus between the inputs
-		self.textinput1.bind(text=self.on_text)
-		self.textinput2.bind(text=self.on_text)
+		# load the dictionary
+		dictionary = Dictionary()
+		dictionary.load_from_file(dictionary_file)
 
-		# Add both TextInput widgets to the layout
-		layout.add_widget(self.textinput1)
-		layout.add_widget(self.textinput2)
+		sm = ScreenManager(transition=FadeTransition()) # Create a ScreenManager with a fade transition effect between screens.
 
-		return layout
+		mainMenù = MainMenuScreen(name ='menu')
+		sm.add_widget(mainMenù) # Add the MainMenuScreen to the ScreenManager and assign it the name 'menu'.
 
-	# Define the on_text method that will switch focus
-	def on_text(self, instance, value):
-		print("on_text")
-		# Switch focus when a letter is typed (i.e., when the length of the text changes)
-		if len(value) > 0:  # A character was typed
-			# Check which TextInput triggered the event and switch focus
-			if instance == self.textinput1:
-				self.textinput2.focus = True  # Set focus on the second TextInput
-			else:
-				self.textinput1.focus = True  # Set focus on the first TextInput
+		gamescreen = GameScreen(dictionary, name = 'game')
+		sm.add_widget(gamescreen)
 
-if __name__ == "__main__":
-	Wordle().run()
+		mainMenù.set_gamescreen(gamescreen)
+
+		sm.add_widget(VictoryScreen(gamescreen, name = 'victory'))
+		sm.add_widget(DefeatScreen(gamescreen, name = 'defeat'))
+
+		return (sm)
+
+
+if __name__ == '__main__':
+	WordleApp().run()
